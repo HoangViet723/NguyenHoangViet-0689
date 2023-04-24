024d880c9e470ef30ca1f40d5e79ae64db10bf89
HoangViet723 <hoangvietn313@gmail.com>
2023-04-24 22:06:05 +0700

    first commit

---
+++ NguyenHoangViet0689/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenHoangViet0689.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
